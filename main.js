31a229b416cc18ed5da2f63c66e7465adb167322
ryan-cariaso <ryanchristophercariaso@gmail.com>
2021-07-20 22:39:19 -0700

    update to html css and js

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 /*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId)=>{
+  const toggle = document.getElementById(toggleId),
+  nav = document.getElementById(navId)
 
+  if(toggle && nav){
+    toggle.addEventListener('click', ()=> {
+      nav.classList.toggle('show')
+    })
+  }
+}
 
+showMenu('nav-toggle','nav-menu')
 /*===== REMOVE MENU =====*/
+const navLink = document.querySelectorAll('.nav_link'),
+  navMenu = document.getElementById('nav-menu')
 
+function linkAction(){
+  navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*===== SCROLL SECTIONS ACTIVE LINK =====*/
 
