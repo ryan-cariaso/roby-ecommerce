9487793ab3467ba68d38f3517a8cdd45b15f7968
ryan-cariaso <ryanchristophercariaso@gmail.com>
2021-07-21 14:49:06 -0700

    update to html, css, js

--- main.js
+++ main.js
@@ -21,6 +21,28 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*===== SCROLL SECTIONS ACTIVE LINK =====*/
+const sections = document.querySelectorAll('section[id]')
 
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+  const scrollY = window.pageYOffset
+
+  sections.forEach(current=>{
+    const sectionHeight = current.offsetHeight
+    const sectionTop = current.offsetTop - 50
+    sectionId = current.getAttribute('id')
+
+    if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+      document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active')
+    }else{
+      document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active')
+    }
+  })
+}
 
 /*===== CHANGE COLOR HEADER =====*/ 
+window.onscroll = () => {
+  const nav = document.getElementById('header')
+  if(this.scrollY >=200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
